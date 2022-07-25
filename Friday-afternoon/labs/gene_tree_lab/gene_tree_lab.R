e40b274cc32e10ad0ed8018f0e57548ea10937bf
Sarah Teichman <teichs@uw.edu>
2022-07-25 19:39:58 -0400

    adding some data

---
+++ Friday-afternoon/labs/gene_tree_lab/gene_tree_lab.R
@@ -0,0 +1,28 @@
+## gene tree visualization - STAMPS 2022 
+## Sarah Teichman, Amy Willis 
+## Friday 29 July 2022 
+
+# --------------------- Introduction -------------------------
+
+# This tutorial will work through visualizing a phylogenomic 
+# tree and gene trees. 
+# The R package to do this analysis is still under construction.
+
+# ------------------ Loading packages and data ---------------
+
+# To analyze trees, we'll use the package `ape` 
+install.packages("ape") # only run this command once 
+
+# To make our gene tree plots, we'll use the package `TreeVizPackage`
+# First make sure that we have `remotes`
+if (!require("remotes", quietly = TRUE))
+  install.packages("remotes") # check that remotes is installed
+remotes::install_github("svteichman/TreeVizPackage")
+
+# We'll start with the output from GToTree, which includes a 
+# phylogenomic tree and gene level alignments. To ask GToTree to 
+# save your gene level alignments, run it with flag -k. 
+
+# First, read in the trees. The output from GToTree has been pre-processed. 
+# 
+
